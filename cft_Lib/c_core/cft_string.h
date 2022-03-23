60c12cbe17799384b34dc4875dbea375d3229c89
TeaCondemns <tea.condemns@gmail.com>
2022-03-23 14:29:57 +0300

    Update cft_string.h

--- cft_Lib/c_core/cft_string.h
+++ cft_Lib/c_core/cft_string.h
@@ -6,6 +6,9 @@
 #include <fcntl.h>
 #include <io.h>
 
+typedef wchar_t _cft_char
+typedef _cft_char* _cft_str
+
 #define _cft_ENCODING_OLD _O_TEXT
 #define _cft_ENCODING_BINARY _O_BINARY
 #define _cft_ENCODING_WIN_U16 _O_WTEXT
@@ -17,8 +20,8 @@
 #define _cft_SET_OUTPUT_ENCODING(_cft_encoding) _setmode(_fileno(stdout), _cft_encoding)
 #define _cft_INIT_OUTPUT _cft_SET_OUTPUT_ENCODING(_cft_ENCODING_DEFAULT)
 
-#define _cft_string(_cft_value) L##_cft_value
-#define _cft_printf(_cft_value, ...) wprintf(_cft_string(_cft_value), __VA_ARGS__)
-#define _cft_print(_cft_value) wprintf(_cft_string(_cft_value))
+#define _cft_init_str(_cft_value) L##_cft_value
+#define _cft_printf(_cft_value, ...) wprintf(_cft_init_str(_cft_value), __VA_ARGS__)
+#define _cft_print(_cft_value) wprintf(_cft_init_str(_cft_value))
 
 #endif
